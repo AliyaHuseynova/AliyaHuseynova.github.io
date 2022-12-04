b118eeaf4bc19a5d040d3ed55f477b77c9440740
AliyaHuseynova <113546368+AliyaHuseynova@users.noreply.github.com>
2022-12-04 16:39:09 +0400

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,7 +1,9 @@
 const card = document.querySelector(".card"),
 inputPart = card.querySelector(".inputP"),  
 textInput = inputPart.querySelector(".text-input"),
-inputField = inputPart.querySelector("input");
+inputField = inputPart.querySelector("input"),
+loc = inputPart.querySelector("button");
+let apiVar;
 
 inputField.addEventListener("keyup", a =>{
     if(a.key == "Enter" && inputField.value != ""){
@@ -9,9 +11,38 @@ inputField.addEventListener("keyup", a =>{
     }
 });
 
+loc.addEventListener("click", () => {
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(successFuction, errorFunction);
+    }else{
+        alert("Not supported by your browser");
+    }
+});
 
-function RequestForApi(city){
-    let apiInput =  `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid={cfe89456982a9a680b9ff3d7eadeb61e}`;
-   fetch(apiInput).then(response => console.log(response.json()));
+function errorFunction(error){
+    textInput.innerText = error.message;
+    textInput.classList.add("error");
 }
 
+
+function successFuction(position){
+    const {latitude, longitude} = position.coordinates;
+    apiVar = 'https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=cfe89456982a9a680b9ff3d7eadeb61e'  
+    fetchData();
+}
+
+
+function RequestForApi(city){
+    apiVar =  `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=cfe89456982a9a680b9ff3d7eadeb61e`;
+    fetchData();
+    }
+
+function fetchData(){
+    textInput.innerText = "The results are getting ready...";
+    textInput.classList.add("pending");
+    fetch(apiVar).then(response => response.json()).then(results => weatherInfo(results));
+}
+
+function weatherInfo(information){
+    console.log(information);
+}
